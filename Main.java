4479fe609d9d32deab472b7ea27606e2c3fd9bab
GermanRaj <137941316+GermanRaj@users.noreply.github.com>
2023-07-06 16:26:46 +0300

    Homework JDK20 _05_07

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String...args) {
+        // zadanije 2/3
+        String text1 = "Sample text1";
+        String text2 = "Sample text2";
+        int number1 = 50;
+        int number2 = 30;
+        int SumOfTwoNumbers = number1+number2;
+        // Slozhenije znachenij
+        int SubtractionOfTwoNumbers = number1-number2;
+        // Vichitanije znachenij
+        int MultiplicationOfTwoNumbers = number1*number2;
+        // Umnozhenije znachenij
+        int DivisionOfTwoNumbers = number1/number2;
+        // Delenije znachenij
+
+                System.out.println(text1);
+                System.out.println(text2);
+                System.out.println(number1);
+                System.out.println(number2);
+                System.out.println(SumOfTwoNumbers);
+                System.out.println(SubtractionOfTwoNumbers);
+                System.out.println(MultiplicationOfTwoNumbers);
+                System.out.println(DivisionOfTwoNumbers);
+
     }
-}
+    }
