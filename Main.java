60ca13b143814206fbfa7763efd1fec5fcd3ef05
GermanRaj <137941316+GermanRaj@users.noreply.github.com>
2023-07-19 23:05:06 +0300

    HomeWork_17_07_PART_2

--- Main.java
+++ Main.java
@@ -1,27 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String...args) {
-        // zadanije 2/3
-        String text1 = "Sample text1";
-        String text2 = "Sample text2";
-        int number1 = 50;
-        int number2 = 30;
-        int SumOfTwoNumbers = number1+number2;
-        // Slozhenije znachenij
-        int SubtractionOfTwoNumbers = number1-number2;
-        // Vichitanije znachenij
-        int MultiplicationOfTwoNumbers = number1*number2;
-        // Umnozhenije znachenij
-        int DivisionOfTwoNumbers = number1/number2;
-        // Delenije znachenij
+    public static void main(String[] args) {
 
-                System.out.println(text1);
-                System.out.println(text2);
-                System.out.println(number1);
-                System.out.println(number2);
-                System.out.println(SumOfTwoNumbers);
-                System.out.println(SubtractionOfTwoNumbers);
-                System.out.println(MultiplicationOfTwoNumbers);
-                System.out.println(DivisionOfTwoNumbers);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Какое у вас имя пользователя?");
+        String name = scanner.nextLine();
+        System.out.println("Привет "+name+"!");
 
     }
-    }
+}
