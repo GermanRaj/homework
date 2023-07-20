4629c32128107fcab5b05f7c081918812626b74f
GermanRaj <137941316+GermanRaj@users.noreply.github.com>
2023-07-20 14:28:46 +0300

    Java_Homework_17_07_part_1

--- Main.java
+++ Main.java
@@ -1,13 +1,29 @@
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
+        int intNumber = 10000;
+        byte byteNumber = 20;
+        double double_number = 3225525.33;
+        float floatNumber = 532252.25F;
+
+        int extensionOfByteToInt = byteNumber;
+        System.out.println("Extension byte to int " +extensionOfByteToInt);
+
+        byte constrictionIntToByte = (byte)intNumber;
+        System.out.println("Constriction int to byte " +constrictionIntToByte);
+
+        double extensionOfFloatToDouble =floatNumber;
+        System.out.println("Extension float to double " +extensionOfFloatToDouble);
+
+        float constrictionOfDoubletoFloat = (float) double_number;
+        System.out.println("Constriction double to float " +constrictionOfDoubletoFloat);
 
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Какое у вас имя пользователя?");
-        String name = scanner.nextLine();
-        System.out.println("Привет "+name+"!");
+
+
+
+
 
     }
 }
